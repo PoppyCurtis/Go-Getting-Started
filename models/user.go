922640ff1019be052ed866c72a422e9964195184
PoppyCurtis <poppy.curtis@gmail.com>
2020-04-19 11:04:55 +0100

    for loops and if conditionals

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 //Supporting package. For sub packages just need directory name
 
 type User struct {
@@ -21,8 +26,40 @@ func getUsers() []*User {
 }
 
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("New user must be set to 0")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
+}
+
+func RemoveUser(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
